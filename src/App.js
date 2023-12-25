a259257cb2bf195ed9737b6297b37fd5f35d7b6d
Gopal Ji <143545132+gopal-codeing@users.noreply.github.com>
2023-12-25 18:12:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+// import StopWatch from './Components/StopWatch/StopWatch.js';
+import Stopwatch from './Stopwatch';
+function App() {
+	return (
+		<div className="App">
+	  <Stopwatch/>
+		</div>
+	);
+}
+
+export default App;
+
